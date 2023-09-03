73d0c71894e2f55498f972b7b214b45f38131120
Auxy6858 <71662994+Auxy6858@users.noreply.github.com>
2023-09-03 04:28:44 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,54 @@
+//   SHIT WE'RE INCLUDING
+#include <iostream>
+#include <string>
+#include <list>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+
+
+
+
+
+
+
+
+
+
+// MAIN FUNCTION LMAO :3
+
+
+
+int main() {
+	
+	std::cout <<
+		   "       ,-------.    , ------.    _____    .-------.  .--.  .--.   .--.   .--------.   .------.\n"
+		<< "       |  .---./    |  .--. |   /     \\   |  .--. |  |  '--'  |   |  |   '--.  .--'   |  [=] |\n"
+		<< "       |  | .--.    |  '--' |  |  .-.  |  |  '--' |  |  .--.  |   |  |      |  |      |   ___'\n"
+		<< "       |  |_|  |    | |---| \\  |  | |  |  |  _____|  |  |  |  |   |  |      |  |      |  |___.\n"
+		<< "       |_______|    |_|   |_|  |__| |__|  |__|       |__|  |__|   |__|      |__|      |______|\n"
+		<< "\n"
+		<< "Lightweight barebones text editor !!!\n";
+
+
+	std::cout << "Enter the directory of the file you want to edit\n";
+	std::string usrin;
+	std::cin >> usrin;
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+	return 0;
+
+}
